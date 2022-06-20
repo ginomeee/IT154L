66fc1cc3f6f18dca29b3dc9726067c9d35477c92
ginomeee <gino.araullo@gmail.com>
2022-06-20 16:39:24 +0800

    Add comments and fix formatting

--- MP01_Araullo_EEA/MP01_Araullo_EEA.cpp
+++ MP01_Araullo_EEA/MP01_Araullo_EEA.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main(){
+  // Input =======================================================
+  system("cls");
   int numproc;
-  cout << "Enter Number of Processes:";
+  cout << "Enter Number of Processes: ";
   cin >> numproc;
   int *arrival = new int[numproc];
   int *burst = new int[numproc];
-  cout << "Arrival Time" << endl;
+  cout << endl << "Arrival Time" << endl;
   for (int i=0;i<numproc;i++){
-    cout << "Enter Process [" << i << "] Arrival Time:";
+    cout << "Enter Process [" << i << "] Arrival Time: ";
     cin >> arrival[i];
   }
-  cout << endl << endl;
+  cout << endl;
   for (int i=0;i<numproc;i++){
-    cout << "Enter Process [" << i << "] Burst Time:";
+    cout << "Enter Process [" << i << "] Burst Time: ";
     cin >> burst[i];
   }
 
-//-----------------------------------------------------
+  // Processing =======================================================
   for(int x=0;x<numproc;x++){
     if (arrival[0] == 1){
 
